f2924c645473b53e93b27a9cd3603f8a89399b7f
I@PRIVACY <104652106+PRIVACY47@users.noreply.github.com>
2023-05-07 10:52:17 +0000

    added backend

--- BulkMailBackend/Controllers/users.js
+++ BulkMailBackend/Controllers/users.js
@@ -3,7 +3,6 @@ const Users = require('../Model/user')
 const getUsers = async (req,res)=>{
     try {
         let users = await Users.find({}).select('email')
-        console.log(users)
         res.json({users:users})
     } catch (error) {
         res.status(500).json({message:"Internal server error"})
